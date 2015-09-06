53f723244e042a8725156aec1fa9ac144e2ebd39
mebobby2 <bobl53@gmail.com>
2015-09-07 10:32:14 +1200

    scale the background image size to the viewsize

--- Source/GameScene.m
+++ Source/GameScene.m
@@ -14,8 +14,9 @@
 -(void)addBackground {
     CGSize viewSize = [CCDirector sharedDirector].viewSize;
     CCSprite *background = [CCSprite spriteWithImageNamed:@"game_scene_bg.png"];
-    
     background.position  = ccp(viewSize.width * 0.5f, viewSize.height * 0.5f);
+    background.scaleX = viewSize.width / [background boundingBox].size.width;
+    background.scaleY = viewSize.height / [background boundingBox].size.height;
     [self addChild:background];
 }
 
